fd0e8cae6ed3c6f53fdbcb710e89bb34e279df88
GannuNb <nbganesh2002@gmail.com>
2026-03-09 10:42:29 +0530

    ourproducts

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import HomeProducts from "../../components/HomeProducts";
+import styles from "../../styles/Products.module.css";
+
+export default function ProductsPage() {
+  return (
+    <main>
+
+
+
+      <HomeProducts />
+
+    </main>
+  );
+}
